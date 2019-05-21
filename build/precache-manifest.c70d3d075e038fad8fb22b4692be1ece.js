384c75de05357815d0d23345d4eac240a29fa006
qiuyu.qiuyu <qiuyu.qiuyu@alibaba-inc.com>
2019-05-21 21:44:20 +0800

    add build

---
+++ build/precache-manifest.c70d3d075e038fad8fb22b4692be1ece.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a0c0e7bce8a5facc64d4e262e55870",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69972d9d319d2db4a75b",
+    "url": "/static/css/main.a53a6a2f.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "69972d9d319d2db4a75b",
+    "url": "/static/js/main.e5ef3e02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
